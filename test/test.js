d849160b49c774a1b73d737084424ee08cefbf99
Justin J. Novack <jnovack@gmail.com>
2018-07-24 05:50:01 -0400

    correct tests

--- test/test.js
+++ test/test.js
@@ -1,17 +1,29 @@
-var start = new Date().getTime();
+/*jshint esversion: 6 */
+
+const fs = require("fs");
+const expect = require('chai').expect;
 const heroprotocol = require('../');
 
-console.log("heroprotocol.js v" + heroprotocol.version);
-console.log("storm-replay v" + heroprotocol.stormVersion);
+describe('test extraction methods', function(){
 
-// You can also do this as:
-//   heroprotocol.extractFiles('alterac-pass.StormReplay');
+    it('javascript extraction', function() {
+        this.originalPlatform = process.platform;
+        Object.defineProperty(process, 'platform', {
+            value: 'win'
+        });
+        for (var i = heroprotocol.FILES.length - 1; i >= 0; i--) {
+            heroprotocol.get(heroprotocol.FILES[i], 'alterac-pass.StormReplay');
+        }
+    });
 
-for (var i = heroprotocol.FILES.length - 1; i >= 0; i--) {
-    heroprotocol.get(heroprotocol.FILES[i], 'alterac-pass.StormReplay');
-}
 
-var stop = new Date().getTime();
-console.log("CPU Time: " + Math.round( ((stop-start)*1000)/1000 )/1000 + " sec");
-const used = process.memoryUsage().heapUsed / 1024 / 1024;
-console.log("Mem Used: " + (Math.round(used * 100) / 100) + " MB");
+    it('storm-replay extraction', function() {
+        Object.defineProperty(process, 'platform', {
+            value: this.originalPlatform
+        });
+        for (var i = heroprotocol.FILES.length - 1; i >= 0; i--) {
+            heroprotocol.get(heroprotocol.FILES[i], 'alterac-pass.StormReplay');
+        }
+    });
+
+});
