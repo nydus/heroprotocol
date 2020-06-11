8a0ffc84264361178aae1d76a851002dc20e3d3f
Justin J. Novack <jnovack@gmail.com>
2020-06-11 08:19:45 -0400

    jslint

--- pino.js
+++ pino.js
@@ -1,8 +1,14 @@
-'use strict'
+/*jshint esversion: 6 */
+/*jslint node: true */
+
+"use strict";
+
 const pino = require('pino');
-const pretty = pino.pretty({forceColor: true})
+const pretty = pino.pretty({forceColor: true});
+
 pretty.pipe(process.stdout);
 var loglevel = process.env.LOGLEVEL || 'warn';
+
 module.exports = pino({
   name: 'hots-parser',
   safe: true,
